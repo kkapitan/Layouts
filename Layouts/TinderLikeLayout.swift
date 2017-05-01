8015cb91f0eef2a46a6c57f4780769b636b86430
Krzysztof <krzysztof.kapitan@gmail.com>
2017-05-01 22:23:18 +0200

    Tinder pt 1

--- Layouts/TinderLikeLayout.swift
+++ Layouts/TinderLikeLayout.swift
@@ -10,7 +10,9 @@ import UIKit
 
 final class TinderLayout: UICollectionViewLayout {
     
-    var attributes: [UICollectionViewLayoutAttributes] = []
+    var cellAttributes: [UICollectionViewLayoutAttributes] = []
+    var decorationAttributes: [UICollectionViewLayoutAttributes] = []
+    
     var maxVisibleItems = 5
     
     var itemSize = CGSize(width: 280, height: 280)
@@ -20,6 +22,11 @@ final class TinderLayout: UICollectionViewLayout {
         super.prepare()
         setupIfNeeded()
         
+        setupCellAttributes()
+        setupDecorationAttributes()
+    }
+    
+    fileprivate func setupCellAttributes() {
         guard let collectionView = collectionView else { return }
         
         let numberOfItems = collectionView.numberOfItems(inSection: 0)
@@ -28,26 +35,108 @@ final class TinderLayout: UICollectionViewLayout {
         let centerX = collectionView.bounds.midX
         let centerY = collectionView.bounds.midY
         
-        attributes = (0..<numberOfVisibleItems).map { index in
+        cellAttributes = (0..<numberOfVisibleItems).map { index in
             
             let indexPath = IndexPath(item: index, section: 0)
             let attribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
             
             attribute.size = itemSize
-            attribute.center = CGPoint(x: centerX, y: centerY - interItemTopOffset * CGFloat(index))
+            
+            let x = centerX
+            let y = centerY - interItemTopOffset * CGFloat(index)
+            
+            let shouldApplyOffset = indexPath.row == 0 && isMoving
+            
+            let offsetX = shouldApplyOffset ? movedOffset.x : x
+            let offsetY = shouldApplyOffset ? movedOffset.y : y
+            
+            attribute.center = CGPoint(x: offsetX, y: offsetY)
             attribute.zIndex = -index
             
             return attribute
         }
     }
     
+    fileprivate func setupDecorationAttributes() {
+        guard let collectionView = collectionView else { return }
+        
+        let size = CGSize(width: 150.0, height: 150.0)
+        
+        let centerX = collectionView.bounds.midX
+        let centerY = collectionView.bounds.midY
+        
+        let acceptIndexPath = IndexPath(item: 1, section: 0)
+        let acceptAttributes = UICollectionViewLayoutAttributes(forDecorationViewOfKind: AcceptView.kind, with: acceptIndexPath)
+        
+        let declineIndexPath = IndexPath(item: 0, section: 0)
+        let declineAttributes = UICollectionViewLayoutAttributes(forDecorationViewOfKind: DeclineView.kind, with: declineIndexPath)
+        
+        let shouldApplyOffset = isMoving
+        
+        let x = shouldApplyOffset ? movedOffset.x : centerX
+        let y = shouldApplyOffset ? movedOffset.y : centerY
+        
+        acceptAttributes.center = CGPoint(x: x, y: y)
+        declineAttributes.center = CGPoint(x: x, y: y)
+        
+        acceptAttributes.zIndex = 100
+        declineAttributes.zIndex = 100
+        
+        acceptAttributes.alpha = min(max(0, (x - centerX)/centerX), 1)
+        declineAttributes.alpha = min(max(0, (centerX - x)/centerX), 1)
+        
+        acceptAttributes.size = size
+        declineAttributes.size = size
+        
+        decorationAttributes = [acceptAttributes, declineAttributes]
+    }
+    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        return attributes
+        return [cellAttributes, decorationAttributes].flatMap { $0 }
     }
     
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        return attributes[indexPath.row]
+        return cellAttributes[indexPath.row]
     }
+    
+    override func layoutAttributesForDecorationView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return decorationAttributes
+            .filter {
+                $0.indexPath == indexPath
+            }.filter {
+                $0.representedElementKind == elementKind
+            }.first
+    }
+    
+    fileprivate var isMoving: Bool = false
+    fileprivate var movedOffset: CGPoint = .zero
+    
+    func handlePan(pan: UIPanGestureRecognizer) {
+        
+        switch pan.state {
+        case .began:
+            
+            let point = pan.location(in: collectionView)
+            let indexPath = collectionView?.indexPathForItem(at: point)
+            isMoving = indexPath?.row == 0
+            
+            fallthrough
+        case .changed:
+            guard isMoving else { return }
+            
+            movedOffset = pan.location(in: collectionView)
+            invalidateLayout()
+            
+        default:
+            if isMoving {
+                invalidateLayout()
+            }
+            
+            isMoving = false
+        }
+        
+    }
+    
     
     fileprivate var contentSize: CGSize = .zero
     override var collectionViewContentSize: CGSize {
@@ -65,6 +154,44 @@ final class TinderLayout: UICollectionViewLayout {
         
         contentSize = CGSize(width: width, height: height)
         
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        collectionView.addGestureRecognizer(panGestureRecognizer)
+        
+        registerDecorationView()
+        
         needsSetup = false
+    }
+    
+    func registerDecorationView() {
+        register(AcceptView.self, forDecorationViewOfKind: AcceptView.kind)
+        register(DeclineView.self, forDecorationViewOfKind: DeclineView.kind)
+    }
+}
+
+final class AcceptView: UICollectionReusableView {
+    static let kind = "AcceptViewKind"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .green
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class DeclineView: UICollectionReusableView {
+    static let kind = "DeclineViewKind"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
